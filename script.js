87bfd16ea4090f0e6bac10daee563f9e43b01fda
Prakhar Tamrakar <prakhartamrakar1@gmail.com>
2023-12-31 22:54:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore() {
+    score += 10;
+    document.querySelector('#scoreval').textContent = score;
+}
+
+function Makebubble() {
+    var clutter = '';
+
+    for (var i = 1; i <= 90; i++) {
+        var rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`
+    }
+
+    document.querySelector('#pbtm').innerHTML = clutter;
+}
+
+function runTimer() {
+    var timeint = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector('#timerval').textContent = timer;
+        } else {
+            clearInterval(timeint);
+            document.querySelector('#pbtm'). innerHTML = `<h1>Game Over</h1>`;    
+        }
+    }, 1000);
+}
+
+function hit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector('#hitval').textContent = hitrn;
+}
+
+document.querySelector('#pbtm')
+    .addEventListener('click', function (dets) {
+        var clickednum = Number(dets.target.textContent);
+        if (clickednum === hitrn) {
+            increaseScore();
+            Makebubble();
+            hit();
+        }
+    });
+
+runTimer();
+Makebubble();
+hit();  
